a805e561476d685f988b034f75fc50e43066713e
mdimapilis <130004953+mdimapilis@users.noreply.github.com>
2024-05-03 18:35:54 -0700

    "update expose.js and add npm"

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,6 +2,49 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
+
 function init() {
+
+  console.log(document.getElementById("horn-select"));
+  console.log(document.querySelector('#expose').childNodes[11]);
   // TODO
+  const hornSelect = document.getElementById("horn-select");
+  const imgSelect = document.querySelector('#expose').childNodes[3];
+  const audioSelect = document.querySelector('#expose').childNodes[11];
+  console.log(audioSelect.src);
+  hornSelect.addEventListener("change", (event) => {
+    imgSelect.src = "assets/images/" + hornSelect.value + ".svg";
+    audioSelect.src = "assets/audio/" + hornSelect.value + ".mp3";
+  });
+
+  const vol = document.getElementById("volume-controls");
+  const volVal = vol.childNodes[1];
+  const volImg = vol.childNodes[3];
+
+  volVal.addEventListener("change", (event) => {
+    const currVol = volVal.value;
+    console.log(currVol);
+    if(currVol == 0) {
+      volImg.src = "assets/icons/volume-level-0.svg";
+    }
+    else if(currVol > 0 && currVol < 33) {
+      volImg.src = "assets/icons/volume-level-1.svg";
+    }
+    else if(currVol > 32 && currVol < 67) {
+      volImg.src = "assets/icons/volume-level-2.svg";
+    }
+    else if(currVol > 66 && currVol <= 100) {
+      volImg.src = "assets/icons/volume-level-3.svg";
+    }
+  });
+
+  const button = document.querySelector('#expose').childNodes[9];
+  const jsConfetti = new JSConfetti();
+  button.addEventListener("click", (event) => {
+    audioSelect.play();
+    if(hornSelect.value == "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
+
 }
